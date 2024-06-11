d6253581695dc19d8ae28f8647a622ad8266d8c6
zbrwkq <damien.chv78@gmail.com>
2024-06-11 16:31:49 +0200

    login functionality

--- front/src/index.js
+++ front/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AuthProvider from "./hooks/AuthProvider";
+import Login from "./routes/auth/login";
+import Topbar from "./components/topbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Topbar />
+    <RouterProvider router={router}>
+      <AuthProvider></AuthProvider>
+    </RouterProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
