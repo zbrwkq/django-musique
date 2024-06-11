45e9672564e9b5b79d9c045232ba8ee3dca1f30f
Barbier Nathan <n.barbier001@gmail.com>
2024-06-11 16:25:45 +0200

    fix User model

--- back/users/models.py
+++ back/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Users(models.Model):
-    email = models.CharField(max_length=200, unique=True, null=False, email=True, blank=False)
+    email = models.EmailField(max_length=200, unique=True, null=False, blank=False)
     password = models.CharField(null=False, blank=False, password=True)
 
     def __str__(self):
