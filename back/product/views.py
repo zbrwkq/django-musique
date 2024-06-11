dd6d924f4c269f9b83436a360e02697f1a6183fc
zbrwkq <damien.chv78@gmail.com>
2024-06-11 10:26:19 +0200

    setup backend

---
+++ back/product/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
+from .filters import ProductsFilter
+
+from .serializers import ProductSerializer
+
+from .models import Product
+
+# Create your views here.
+
+@api_view(['GET'])
+def get_products(request):
+
+    filterset = ProductsFilter(request.GET, queryset=Product.objects.all().order_by('id'))
+
+    count = filterset.qs.count()
+
+    # Pagination
+    resPerPage = 1
+
+    paginator = PageNumberPagination()
+    paginator.page_size = resPerPage
+
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+
+    serializer = ProductSerializer(queryset, many=True)
+
+    return Response({ 
+        "count": count,
+        "resPerPage": resPerPage,
+        "products": serializer.data
+         })
+
+
+@api_view(['GET'])
+def get_product(request, pk):
+
+    product = get_object_or_404(Product, id=pk)
+
+    serializer = ProductSerializer(product, many=False)
+
+    return Response({ "product": serializer.data })
