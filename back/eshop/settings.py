3023eb4a2b02616202d9d0b3af1bd74f0fbb61c7
zbrwkq <81628036+zbrwkq@users.noreply.github.com>
2024-06-12 16:55:03 +0200

    fix auth (#25)

--- back/eshop/settings.py
+++ back/eshop/settings.py
@@ -55,9 +55,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
       'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
 }
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
--- back/tracks/views.py
+++ back/tracks/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import render
 
 from .serializers import TracksSerializer
@@ -10,6 +11,7 @@ from .models import Tracks
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_tracks(request):
     tracks = Tracks.objects.all()
 
