14fbf0514d881a61c814b1f02ee9e12e70badd3c
zbrwkq <damien.chv78@gmail.com>
2024-06-13 20:54:20 +0200

    ptit style

--- front/src/routes/pages/home/home.jsx
+++ front/src/routes/pages/home/home.jsx
@@ -4,87 +4,95 @@ import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
-    <Container>
-      <h1 className="m-5 d-flex align-items-center flex-column">
-        Suivez les musiques que vous avez écouté.
-        <br />
-        Lisez des avis sur vos musiques préférées.
-        <br />
-        Recommandez des albums à vos amis.
-      </h1>
-      <NavLink
-        href="/register"
-        className="d-flex align-items-center flex-column"
+    <>
+      <section
+        id="preview-page"
+        className="p-5 mb-3 d-flex flex-column justify-content-end"
+        style={{ height: "90vh" }}
       >
-        <Button className="my-3 w-25">S'inscrire maintenant</Button>
-      </NavLink>
-      <div>
-        <Row className="mb-3">
-          <Col>
-            <Card className="summary-card">
-              <Card.Body>
-                <Card.Text>
-                  Gardez une trace de toutes les musiques que vous avez écoutés
-                  (ou commencez simplement à partir du jour de votre
-                  inscription)
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="summary-card">
-              <Card.Body>
-                <Card.Text>
-                  Montrez un peu d'amour pour vos albums et titres préférés avec
-                  un « j'aime »
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="summary-card">
-              <Card.Body>
-                <Card.Text>
-                  Écrivez et partagez des avis, et suivez vos amis et d'autres
-                  membres pour lire les leurs
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <Row className="mb-3">
-          <Col>
-            <Card className="summary-card">
-              <Card.Body>
-                <Card.Text>
-                  Évaluez chaque album sur une échelle de cinq étoiles pour
-                  enregistrer et partager votre réaction
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="summary-card">
-              <Card.Body>
-                <Card.Text>
-                  Tenez un journal de vos écoutes de musiques
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="summary-card">
-              <Card.Body>
-                <Card.Text>
-                  Partagez cette application avec vos amis pour leur montrer à
-                  quel point vous avez bon goûts
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    </Container>
+        <h1 className="mb-5 d-flex align-items-center flex-column lh-lg">
+          Suivez les musiques que vous avez écouté.
+          <br />
+          Lisez des avis sur vos musiques préférées.
+          <br />
+          Recommandez des albums à vos amis.
+        </h1>
+        <NavLink
+          to="/register"
+          className="mb-5 d-flex align-items-center flex-column"
+        >
+          <Button className="w-25">S'inscrire maintenant</Button>
+        </NavLink>
+      </section>
+      <Container>
+        <div>
+          <Row className="mb-3">
+            <Col>
+              <Card className="summary-card">
+                <Card.Body>
+                  <Card.Text>
+                    Gardez une trace de toutes les musiques que vous avez
+                    écoutés (ou commencez simplement à partir du jour de votre
+                    inscription)
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="summary-card">
+                <Card.Body>
+                  <Card.Text>
+                    Montrez un peu d'amour pour vos albums et titres préférés
+                    avec un « j'aime »
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="summary-card">
+                <Card.Body>
+                  <Card.Text>
+                    Écrivez et partagez des avis, et suivez vos amis et d'autres
+                    membres pour lire les leurs
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col>
+              <Card className="summary-card">
+                <Card.Body>
+                  <Card.Text>
+                    Évaluez chaque album sur une échelle de cinq étoiles pour
+                    enregistrer et partager votre réaction
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="summary-card">
+                <Card.Body>
+                  <Card.Text>
+                    Tenez un journal de vos écoutes de musiques
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="summary-card">
+                <Card.Body>
+                  <Card.Text>
+                    Partagez cette application avec vos amis pour leur montrer à
+                    quel point vous avez bon goûts
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </>
   );
 };
 
